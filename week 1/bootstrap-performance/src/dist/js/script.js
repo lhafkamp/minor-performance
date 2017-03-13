d12ccfbdbbbed4bd2fbfbbe65489df1fa08b1c88
lhafkamp <Luuk@218.225.028.145.hva.nl>
2017-03-13 15:15:28 +0100

    removed unwanted fonts, installed fontfaceobserver

---
+++ week 1/bootstrap-performance/src/dist/js/script.js
@@ -0,0 +1,8 @@
+const FontFaceObserver = require('fontfaceobserver');
+const font = new FontFaceObserver('source_sans_pro');
+
+font.load().then(() => {
+  console.log('Font has loaded.');
+}).catch(() => {
+  console.log('Font failed to load.');
+});
